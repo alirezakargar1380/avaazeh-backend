5cea55f066187899b8ae4e72dbf4c5695fdc9cdf
alireza kargar <imanesche@gmail.com>
2023-08-10 14:43:20 +0330

    complete packages section

--- src/package/package.controller.ts
+++ src/package/package.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Get, Res, HttpStatus, Post, Param, Put } from '@nestjs/common';
+import { PackageService } from './package.service';
+import { error_response } from 'src/shared/response/response';
+import { Response } from 'express';
 
 @Controller('package')
-export class PackageController {}
+export class PackageController {
+    constructor(
+        private readonly packageService: PackageService
+    ) { }
+
+    @Post()
+    async addPackage(@Body() body, @Res() res: Response) {
+        try {
+            /**
+             * TODO:
+             *    - CHECK USER IS ADMIN OR NOT
+             */
+            res.status(HttpStatus.ACCEPTED).send(await this.packageService.save(body))
+        } catch (e) {
+            error_response(e, res)
+        }
+    }
+
+    @Get()
+    async getPackages(@Body() body, @Res() res: Response) {
+        try {
+            const packages = await this.packageService.getAll()
+
+            res.status(HttpStatus.ACCEPTED).send(packages)
+        } catch (e) {
+            error_response(e, res)
+        }
+    }
+
+    @Put('/:id/edit')
+    async updatePage(@Param('id') id: string, @Body() body: any, @Res() res: Response) {
+        try {
+            const result = await this.packageService.update(Number(id), body)
+            res.status(HttpStatus.CREATED).send(result);
+        } catch (e) {
+            error_response(e, res)
+        }
+    }
+}
--- src/package/package.service.ts
+++ src/package/package.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Package } from './entitys/package.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PackageService {}
+export class PackageService {
+    constructor(
+        @InjectRepository(Package) private packageRepository: Repository<Package>
+    ) { }
+
+    save(data: any) {
+        return this.packageRepository.save(data)
+    }
+
+    getAll() {
+        return this.packageRepository.find()
+    }
+
+    update(id: number, data: any): Promise<any> {
+        return this.packageRepository.update({
+            id: id
+        }, data)
+    }
+}
